12185e55b60e27f0cd362e8c79aaf6e5498dda25
Tomas Aranda <tomasaranda2018@gmail.com>
2022-08-02 15:13:02 -0300

    change card size

--- components/contentMain/cards/Cards.jsx
+++ components/contentMain/cards/Cards.jsx
@@ -5,7 +5,7 @@ import Icons, {icons} from '../../icons/Icons'
 const Cards = ({ammout, text}) => {
   return (
 
-    <div className="bg-white shadow-sm rounded-lg p-3 sm:p-6  border-2 border-x border-t-0 w-full">
+    <div className="bg-white shadow-sm rounded-lg p-3 sm:p-6 sm:py-5  border-2 border-x border-t-0 w-full">
         <div className="flex justify-between items-center">
             <p className="text-sm text-gray-500 font-medium">{text}</p>
             <Icons icon_type={icons.dotsIcon} className=" text-xl sm:text-2xl text-gray-400"/>
